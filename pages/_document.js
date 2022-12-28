77c03a57bbab70c18f2b25207d4f752b7f95f328
eduardo.sa.lima <eduardo.lima@sysvale.com>
2022-12-28 15:09:17 -0300

    adiciona bootstrap

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
+ 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+ return (
+   <Html>
+     <Head>
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" />
+       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+     </Head>
+     <body>
+       <Main />
+       <NextScript />
+     </body>
+   </Html>
+ )
 }
